101b7257d2836ac261390cd26356ec309ca151be
VasaBhanuPrakash <125045216+VasaBhanuPrakash@users.noreply.github.com>
2023-10-15 21:54:39 +0530

    Update locations.js

--- app_api/controller/locations.js
+++ app_api/controller/locations.js
@@ -2,6 +2,8 @@ var sendJsonResponse = function(res, status, content) {
     res.status(status);
     res.json(content);
 };
+var mongoose=require("mongoose")
+const Loc=mongoose.model("Location")
 module.exports.locationsCreate = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
@@ -12,8 +14,8 @@ var mongoose = require('mongoose');
 const locationSchema = new mongoose.Schema({
     name: String,
   });
-const Loc = mongoose.model('Location',locationSchema);
-module.exports.locationsReadOne = async function (req, res) {
+ 
+module.exports.locationsReadOne = function (req, res) {
 try {
 const location = await
 Loc.findById(req.params.locationid).exec();
@@ -24,9 +26,27 @@ console.log(location)
 sendJsonResponse(res, 500, { error: 'An error occurred' });
 }
 };
-module.exports.locationsUpdateOne = function (req, res) {
-    sendJsonResponse(res, 200, {"status" : "success"});
+module.exports.locationsUpdateOne = async function (req, res) {
+    try {
+        const location = await Loc.findById(req.params.locationid).exec();
+
+        if (!location) {
+            sendJsonResponse(res, 404, { error: 'Location not found' });
+            return;
+        }
+        if (req.body) {
+            if (req.body.author) location.author = req.body.author;
+            if (req.body.rating) location.rating = req.body.rating;
+            if (req.body.reviewText) location.reviewText = req.body.reviewText;
+        }
+        await location.save();
+        sendJsonResponse(res, 200, location);
+    } catch (err) {
+        // Handle the error
+        sendJsonResponse(res, 500, { error: 'An error occurred' });
+    }
 };
+
 module.exports.locationsDeleteOne = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
